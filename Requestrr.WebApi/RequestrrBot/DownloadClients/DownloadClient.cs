8075a4222a6faacbd9855421f644cada7df329c4
polyjitter <polyjitter@fastmail.com>
2024-11-03 15:36:09 -0500

    Readarr stuff give me a sec

--- Requestrr.WebApi/RequestrrBot/DownloadClients/DownloadClient.cs
+++ Requestrr.WebApi/RequestrrBot/DownloadClients/DownloadClient.cs
@@ -8,5 +8,6 @@ namespace Requestrr.WebApi.RequestrrBot.DownloadClients
         public const string Ombi = "Ombi";
         public const string Overseerr = "Overseerr";
         public const string Lidarr = "Lidarr";
+        public const string Readarr = "Readarr";
     }
 }
